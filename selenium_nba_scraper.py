bb8dbd0ed63b9c623931efa10c5b069f659498d7
Jeremy <jeray3000@gmail.com>
2022-04-30 16:40:55 -0400

    Finished

--- selenium_nba_scraper.py
+++ selenium_nba_scraper.py
@@ -49,7 +49,8 @@ class Bot_scraper:
         self.driver.get(self.url)      
         self.actions = ActionChains(self.driver) 
         self.loop_counter = 0
-        self.year_dict = {}
+        self.year_dict_winners = {}
+        self.year_dict_losers = {}
 
         
     def get_winners_and_losers(self):
@@ -81,10 +82,10 @@ class Bot_scraper:
 
     def get_team_stats(self, team, year="2020"):
         logger.info("Getting team stats")
-        self.url = 'https://www.basketball-reference.com/playoffs/NBA_' + str(year) + '.html' 
-        self.driver.get(self.url)  
+        logger.info ("url: " + self.url)
+        # For 100 pos stats
         #//div[@id='all_totals_team-opponent']//td/a[contains(text(), 'Dallas')]/../../.
-        row_xpath = "//div[@id='all_totals_team-opponent']//td/a[contains(text(), '" + team + "')]/../../*" # This gives all the children in the xpath
+        row_xpath = "//div[@id='switcher_per_poss_team-opponent']//td/a[contains(text(), '" + team + "')]/../../*" # This gives all the children in the xpath
         try:
             WebDriverWait(self.driver, 5).until(
                 EC.presence_of_element_located((By.XPATH, row_xpath))
@@ -96,7 +97,27 @@ class Bot_scraper:
             logger.exception("Stats fail to capture")
         list_of_stats = []
 
-        for index in range(25): #There are 25 stats we want. The xpath finds the opponent stats too and we don't want that so just manually setting length to 25.
+        for index in range(24): #There are 24 stats we want. The xpath finds the opponent stats too and we don't want that so just manually setting length to 25.
+            list_of_stats.append(stat_elements[index].text)
+
+        
+        #For advanced stats
+        row_xpath2 = "//div[@id='div_advanced-team']//td/a[contains(text(), '" + team + "')]/../../*" # This gives all the children in the xpath
+        logger.info("Row_xpath2: " + row_xpath2)
+        try:
+            WebDriverWait(self.driver, 5).until(
+                EC.presence_of_element_located((By.XPATH, row_xpath))
+            )
+            stat_elements = self.driver.find_elements(By.XPATH, row_xpath2 )
+            logger.info(stat_elements)
+
+            logger.info("Stats captured 2 ")
+        except Exception as e:
+            logger.error(e)
+            logger.exception("Stats fail to capture")
+
+        for index in range(len(stat_elements)): 
+            logger.info(stat_elements[index].text)
             list_of_stats.append(stat_elements[index].text)
 
         return list_of_stats
@@ -106,14 +127,33 @@ class Bot_scraper:
         self.url = 'https://www.basketball-reference.com/playoffs/NBA_' + str(year) + '.html' 
         self.driver.get(self.url) 
         winners_and_losers = self.get_winners_and_losers()
-        self.year_dict[year] = {}
+        self.url = 'https://www.basketball-reference.com/leagues/NBA_' + str(year) + '.html' 
+        self.driver.get(self.url) 
+        self.year_dict_winners[year] = []
+        self.year_dict_losers[year] = []
+        for team in winners_and_losers[1]:
+            team_stats = self.get_team_stats(team, year)
+            self.year_dict_winners[year].append(team_stats)
+
         for team in winners_and_losers[0]:
             team_stats = self.get_team_stats(team, year)
-            self.year_dict[year][team] = team_stats
+            self.year_dict_losers[year].append(team_stats)
+
+    def create_csv(self,  year):
+        with open("csv_folder\\winner_" + str(year) + ".txt", 'w') as csv:
+            for team in self.year_dict_winners[year]:
+                for stat in team:
+                    csv.write(stat + ",")
+                csv.write("\n")
+
+        with open("csv_folder/loser_" + str(year) + ".txt", 'w') as csv:           
+            for team in self.year_dict_losers[year]:
+                for stat in team:
+                    csv.write(stat + ",")
+                csv.write("\n")
 
 if __name__ == '__main__':
     web_scraper = Bot_scraper()
-    #for year in range(2017, 2018):
-    #    web_scraper.get_stats_for_year(year)
-    web_scraper.get_stats_for_year(2020)
-    logger.info(web_scraper.year_dict)
+    for year in range(1990, 2020):
+        web_scraper.get_stats_for_year(year)
+        web_scraper.create_csv(year)
